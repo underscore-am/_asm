19e82244288f3734b580eba06f8a44a849c2e426
Vahagn <metavahagn@gmail.com>
2023-10-10 01:08:16 +0400

    feat: add new compiler

--- src/sections/Editor.tsx
+++ src/sections/Editor.tsx
@@ -1,17 +1,15 @@
 import { onMount } from "solid-js";
 import { loadMonaco } from "../utils/editor";
 import { compileCode } from "../utils/compiler";
+import { compile } from "../utils/compiler-v2";
 
 
 let monacoContainer: HTMLDivElement;
 export function Editor() {
 	onMount(() => {
 		loadMonaco(monacoContainer, "");
-		console.log(compileCode(`
-		move R1 R2
-		@start
-		add R1 R2
-		jumpeq start
+		console.log(compile(`
+		move r1 r2
 		`));
 	});
 
