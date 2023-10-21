8d2067cf51731c0ccca5c8ca3cd12574d603f69b
Vahagn <metavahagn@gmail.com>
2023-10-21 14:53:14 +0400

    feat: export colors

--- src/utils/editor.tsx
+++ src/utils/editor.tsx
@@ -131,3 +131,28 @@ export async function loadMonaco(
     editor: monaco.editor,
   };
 }
+
+export const BACKGROUND = "080808";
+export const FOREGROUND = "bdbdbd";
+export const BOLD = "eeeeee";
+export const CURSOR = "9e9e9e";
+export const CURSOR_TEXT = "080808";
+export const SELECTION = "b2ceee";
+export const SELECTION_TEXT = "080808";
+export const BLACK_NORMAL = "323437";
+export const RED_NORMAL = "ff5454";
+export const GREEN_NORMAL = "8cc85f";
+export const YELLOW_NORMAL = "e3c78a";
+export const BLUE_NORMAL = "80a0ff";
+export const PURPLE_NORMAL = "cf87e8";
+export const CYAN_NORMAL = "79dac8";
+export const WHITE_NORMAL = "c6c6c6";
+export const BLACK_BRIGHT = "949494";
+export const RED_BRIGHT = "ff5189";
+export const GREEN_BRIGHT = "36c692";
+export const YELLOW_BRIGHT = "c2c292";
+export const BLUE_BRIGHT = "74b2ff";
+export const PURPLE_BRIGHT = "ae81ff";
+export const CYAN_BRIGHT = "85dc85";
+export const WHITE_BRIGHT = "e4e4e4";
+
