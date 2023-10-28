c1e6dbc017b6fec68d1d907a0d5fb8fea3ac5b33
Artyom <artyom.karapetyan@zoomerang.app>
2023-10-28 13:27:40 +0400

    fix: fix background color

--- src/sections/Page.tsx
+++ src/sections/Page.tsx
@@ -14,158 +14,154 @@ export const RAM_SECTION_BLOCKS = Math.pow(2, 8);
 let monacoContainer: HTMLDivElement;
 
 export function Page() {
-	const [editor, setEditor] = createSignal<editor.IStandaloneCodeEditor | null>(
-		null
-	);
-	const [vm, setVm] = createSignal<VM>();
-	const [ram, setRam] = createSignal<number[]>();
-	const [registers, setRegisters] = createSignal<number[]>();
-	const [ramPointer, setRamPointer] = createSignal<number>();
-	const [instructionResult, setInstructionResult] =
-		createSignal<InstructionResult>();
+  const [editor, setEditor] = createSignal<editor.IStandaloneCodeEditor | null>(
+    null
+  );
+  const [vm, setVm] = createSignal<VM>();
+  const [ram, setRam] = createSignal<number[]>();
+  const [registers, setRegisters] = createSignal<number[]>();
+  const [ramPointer, setRamPointer] = createSignal<number>();
+  const [instructionResult, setInstructionResult] =
+    createSignal<InstructionResult>();
 
-	const [e, setE] = createSignal<typeof editorNs>();
+  const [e, setE] = createSignal<typeof editorNs>();
 
-	function refreshAddresses(vm: VM, result?: InstructionResult) {
-		let address = ramPointer();
-		if (address === undefined) {
-			setRamPointer(0);
-			address = 0;
-		}
+  function refreshAddresses(vm: VM, result?: InstructionResult) {
+    let address = ramPointer();
+    if (address === undefined) {
+      setRamPointer(0);
+      address = 0;
+    }
 
-		const ramSection = vm.memory.slice(address, address + RAM_SECTION_BLOCKS);
-		setRam(Array.from(ramSection));
-		setRegisters(Array.from(vm.registers));
-		setInstructionResult(result);
+    const ramSection = vm.memory.slice(address, address + RAM_SECTION_BLOCKS);
+    setRam(Array.from(ramSection));
+    setRegisters(Array.from(vm.registers));
+    setInstructionResult(result);
 
-		if (!result) {
-			return;
-		}
+    if (!result) {
+      return;
+    }
 
-		const instruction = vm.registers[registerNameToBinary.ip!]
-		const line = vm.maps.get(instruction);
+    const instruction = vm.registers[registerNameToBinary.ip!];
+    const line = vm.maps.get(instruction);
 
+    const model = editor()!.getModel()!;
 
-		const model = editor()!.getModel()!;
+    let next = [
+      {
+        startLineNumber: line ?? 0,
+        endLineNumber: line ?? 0,
+        startColumn: 1,
+        endColumn: 1000,
+        message: "Next instruction",
+        severity: 2,
+      },
+    ];
 
-		let next = [{
-			startLineNumber: line ?? 0,
-			endLineNumber: line ?? 0,
-			startColumn: 1,
-			endColumn: 1000,
-			message: "Next instruction",
-			severity: 2,
-		}]
+    if (line === undefined) {
+      next = [];
+    }
 
-		if (line === undefined) {
-			next = [];
-		}
+    e()!.setModelMarkers(model!, "myOwner", next);
+  }
 
-		e()!.setModelMarkers(
-			model!,
-			"myOwner",
-			next,
-		);
-	}
+  onMount(() => {
+    loadMonaco(monacoContainer, `move r0 1 \nadd r0 1010`).then((result) => {
+      setEditor(result.myEditor);
+      result.editor;
+      setE(result.editor);
+    });
+  });
 
+  function handleRun() {
+    const code = vm();
+    console.log(code);
+    if (!code) {
+      return;
+    }
 
-	onMount(() => {
-		loadMonaco(monacoContainer, `move r0 1 \nadd r0 1010`).then((result) => {
-			setEditor(result.myEditor);
-			result.editor;
-			setE(result.editor);
-		});
-	});
+    const result = run(code);
+    refreshAddresses(code, result);
+    console.log(result);
+  }
 
-	function handleRun() {
-		const code = vm();
-		console.log(code);
-		if (!code) {
-			return;
-		}
+  function handleCompile() {
+    const E = editor();
+    if (!E) {
+      return;
+    }
+    const value = E.getValue();
 
-		const result = run(code);
-		refreshAddresses(code, result);
-		console.log(result);
-	}
+    const result = compile(value);
+    if (iserr(result)) {
+      console.log(result.data);
+      const model = E.getModel();
 
-	function handleCompile() {
-		const E = editor();
-		if (!E) {
-			return;
-		}
-		const value = E.getValue();
+      e()?.setModelMarkers(
+        model!,
+        "myOwner",
+        result.data.map((error) => {
+          return {
+            startLineNumber: error.line + 1, // line number where the error starts
+            endLineNumber: error.line + 1, // line number where the error ends
+            startColumn: 1, // start column of the error
+            endColumn: 1000, // end column of the error
+            message: error.message,
+            severity: 8, // Severity level
+          };
+        })
+      );
+      return;
+    }
 
-		const result = compile(value);
-		if (iserr(result)) {
-			console.log(result.data);
-			const model = E.getModel();
+    const model = E.getModel();
+    e()?.setModelMarkers(model!, "myOwner", []);
 
-			e()?.setModelMarkers(
-				model!,
-				"myOwner",
-				result.data.map((error) => {
-					return {
-						startLineNumber: error.line + 1, // line number where the error starts
-						endLineNumber: error.line + 1, // line number where the error ends
-						startColumn: 1, // start column of the error
-						endColumn: 1000, // end column of the error
-						message: error.message,
-						severity: 8, // Severity level
-					};
-				})
-			);
-			return;
-		}
+    const vm = init(result.data);
+    setVm(vm);
+    refreshAddresses(vm);
+  }
 
-		const model = E.getModel();
-		e()?.setModelMarkers(model!, "myOwner", []);
+  createEffect(() => {
+    let address = ramPointer();
+    if (address === undefined) {
+      return;
+    }
+    const start = address * RAM_SECTION_BLOCKS;
+    const ramSection = vm()?.memory.slice(start, start + RAM_SECTION_BLOCKS);
 
-		const vm = init(result.data);
-		setVm(vm);
-		refreshAddresses(vm);
-	}
+    if (!ramSection) {
+      return;
+    }
 
-	createEffect(() => {
-		let address = ramPointer();
-		if (address === undefined) {
-			return;
-		}
-		const start = address * RAM_SECTION_BLOCKS;
-		const ramSection = vm()?.memory.slice(start, start + RAM_SECTION_BLOCKS);
+    setRam(Array.from(ramSection));
+  });
 
-		if (!ramSection) {
-			return;
-		}
-
-		setRam(Array.from(ramSection));
-	});
-
-	return (
-		<div
-			class="flex h-[100vh] w-full overflow-hidden gap-[6px] bg-black"
-			onKeyDown={(e) => {
-				if (e.key === "s" && (e.metaKey || e.ctrlKey)) {
-					e.preventDefault();
-					handleCompile();
-					console.log("adad");
-				}
-			}}
-		>
-			<Editor
-				handleRun={handleRun}
-				handleCompile={handleCompile}
-				shouldDisable={() => instructionResult()?.shouldHalt ?? false}
-				ref={monacoContainer}
-			/>
-			<Ram
-				registers={registers}
-				section={ram}
-				pointer={ramPointer}
-				result={instructionResult}
-				setRamPointer={setRamPointer}
-			/>
-			<Regs registers={registers} result={instructionResult} />
-		</div>
-	);
+  return (
+    <div
+      class="flex h-[100vh] w-full overflow-hidden gap-[6px] bg-[#000]"
+      onKeyDown={(e) => {
+        if (e.key === "s" && (e.metaKey || e.ctrlKey)) {
+          e.preventDefault();
+          handleCompile();
+          console.log("adad");
+        }
+      }}
+    >
+      <Editor
+        handleRun={handleRun}
+        handleCompile={handleCompile}
+        shouldDisable={() => instructionResult()?.shouldHalt ?? false}
+        ref={monacoContainer}
+      />
+      <Ram
+        registers={registers}
+        section={ram}
+        pointer={ramPointer}
+        result={instructionResult}
+        setRamPointer={setRamPointer}
+      />
+      <Regs registers={registers} result={instructionResult} />
+    </div>
+  );
 }
